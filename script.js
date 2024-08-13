339a666a7460b498bbb699370c6252ead26f2881
zakygz651 <153487636+zakygz651@users.noreply.github.com>
2024-08-13 19:01:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function showDescription(project) {
   
   switch (project) {
     case 'project1':
-      description = 'Project 1: Ini adalah aplikasi web sederhana yang saya buat menggunakan HTML, CSS, dan JavaScript. Aplikasi ini memiliki fitur interaktif dan desain responsif.';
+      description = 'Auto Fill & Spam Formulir, Projek Ini adalah hasil iseng saya';
       break;
     case 'project2':
-      description = 'Project 2: Pada proyek ini, saya mengembangkan sistem manajemen konten yang memungkinkan pengguna untuk mengelola konten mereka secara efisien. Proyek ini dibangun dengan PHP dan MySQL.';
+      description = 'Project 2: WhatsAppBot, Projek ini adalah projek yang menurut saya sangat bagus sekali,karena bisa mengasah skil ngoding saya';
       break;
     case 'project3':
-      description = 'Project 3: Ini adalah proyek bot otomatis untuk aplikasi chat yang dibuat menggunakan Node.js. Bot ini mampu melakukan berbagai tugas seperti auto-reply dan pengumpulan data.';
+      description = 'Project 3: DDoS Attack,Tools Ini adalah tools yang saya buat.saya tidak ingin mempublish tools ini karena merugikan';
       break;
   }
   
